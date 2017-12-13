3038ca911cfbfa162826531977fb44649a022097
jdpoccorie <juandiego.poccori@gmail.com>
2017-12-13 01:15:32 -0500

    firebase social providers fb

--- app.js
+++ app.js
@@ -1,103 +1,101 @@
 $(function () {
-
-	$('#login-form-link').click(function (e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function (e) {
-		$("#register-form").delay(100).fadeIn(100);
-		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-	$('.fbLogo').click(fbLogin);
-
+  $('#login-form-link').click(function (e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function (e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
 });
 
-var sendEmail = function () {
-	var user = firebase.auth().currentUser;
+let sendEmail = () => {
+  let user = firebase.auth().currentUser // quien es el usuario activo
 
-	user.sendEmailVerification()
-		.then(function () {
-			console.log('El correo se envió');
-		}, function (error) {
-			console.log(error)
-		})
+  user.sendEmailVerification()
+    .then(() => {
+      console.log('El correo se envio')
+    }, (err) => {
+      console.log( err )
+    })
 }
 
 
+// createUser
+let createUser = () => {
+  let email = $('#email-new').val(),
+    password = $('#password-new').val()
 
-//Create User
-var createUser = function () {
-	var email = $('#email-new').val();
-	var password = $('#password-new').val();
-
-	firebase.auth().createUserWithEmailAndPassword(email, password)
-		.then(function (data) {
-			console.log(data)
-			sendEmail();
-		})
-		.catch(function (error) {
-			console.log(error)
-		})
-	getUser();
-	return false;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(( data ) => {
+      console.log( data )
+      sendEmail()
+    })
+    .catch((err) => {
+      console.error( err )
+    })
+  getUser()
+  return false
 }
 
-
-//Get User
-var getUser = function () {
-	firebase.auth().onAuthStateChanged(function (user) {
-		if (user) {
-			console.log(user)
-			$('.saludo').html('Tu usuario es: <b>' + user.email + '</b>')
-			$('#access').hide();
-			$('#logged').show();
-		} else {
-			$('#access').show();
-			$('#logged').hide();
-		}
-	})
+// getUser
+let getUser = () => {
+  firebase.auth().onAuthStateChanged(( user ) => {
+    // existe el usuario?
+    if ( user ) {
+      console.log( user )
+      $('.saludo').html(`Tu usuario es: <b>${ user.email }</b>`)
+      $('#access').hide()
+      $('#logged').show()
+    } else {
+      $('#access').show()
+      $('#logged').hide()
+    }
+  })
 }
 
-getUser();
+getUser()
 
-var login = function () {
-	var email = $('#email').val();
-	var password = $('#password').val();
+let login = () => {
+  let email     = $('#email').val(),
+      password  = $('#password').val()
 
-	firebase.auth().signInWithEmailAndPassword(email, password)
-		.catch(function (error) {
-			console.log(error)
-		})
+  firebase.auth().signInWithEmailAndPassword( email, password )
+    .catch(( err ) => {
+      console.log( err )
+    })
 }
+// Nota: onAuthStateChanged isgue activo, ya que es parte de toda la instancia de Firebase ojo
 
-var logout = function () {
-	firebase.auth().signOut()
-		.then(function () {
-			console.log('Ya terminó la sesión')
-		}, function (error) {
-			console.log(error);
-		})
+
+let logout = () => {
+  firebase.auth().signOut()
+  .then(() => {
+    // todo salio correctamente
+    console.log('Ya termino la sesión')
+  }, (err) => {
+    console.log( err )
+  })
 }
+// Nota: onAuthStateChanged isgue activo, ya que es parte de toda la instancia de Firebase ojo
 
-var recoverPass = function () {
-	var auth = firebase.auth();
-	var emailAddress = $('#email').val();
-
-	auth.sendPasswordResetEmail(emailAddress)
-		.then(function () {
-			alert('Se ha enviado un correo a su cuenta. Por favor sigue los pasos indicados.');
-		}, function (error) {
-			console.log(error)
-		})
+let recoverPass = () => {
+  let auth          = firebase.auth(),
+      emailAddress  = $('#email').val()
+  
+  auth.sendPasswordResetEmail( emailAddress )
+    .then(() => {
+      alert('Se ha enviado un correo a su cuenta, Por favor sigue los pasos indicados')
+    }, ( err ) => {
+      console.log( err )
+    })
 }
-
 
 var fbLogin = function () {
 	//Facebook login actions...
