83d9244e70ec8f81a5dab4f9cdad9ac8f0324e33
jdpoccorie <juandiego.poccori@gmail.com>
2017-12-13 01:03:01 -0500

    firebase social providers

--- app.js
+++ app.js
@@ -1,46 +1,104 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+$(function () {
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+	$('#login-form-link').click(function (e) {
+		$("#login-form").delay(100).fadeIn(100);
+		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function (e) {
+		$("#register-form").delay(100).fadeIn(100);
+		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
 
-var app = express();
+	$('.fbLogo').click(fbLogin);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var sendEmail = function () {
+	var user = firebase.auth().currentUser;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+	user.sendEmailVerification()
+		.then(function () {
+			console.log('El correo se envió');
+		}, function (error) {
+			console.log(error)
+		})
+}
 
-module.exports = app;
+
+
+//Create User
+var createUser = function () {
+	var email = $('#email-new').val();
+	var password = $('#password-new').val();
+
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then(function (data) {
+			console.log(data)
+			sendEmail();
+		})
+		.catch(function (error) {
+			console.log(error)
+		})
+	getUser();
+	return false;
+}
+
+
+//Get User
+var getUser = function () {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			console.log(user)
+			$('.saludo').html('Tu usuario es: <b>' + user.email + '</b>')
+			$('#access').hide();
+			$('#logged').show();
+		} else {
+			$('#access').show();
+			$('#logged').hide();
+		}
+	})
+}
+
+getUser();
+
+var login = function () {
+	var email = $('#email').val();
+	var password = $('#password').val();
+
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.catch(function (error) {
+			console.log(error)
+		})
+}
+
+var logout = function () {
+	firebase.auth().signOut()
+		.then(function () {
+			console.log('Ya terminó la sesión')
+		}, function (error) {
+			console.log(error);
+		})
+}
+
+var recoverPass = function () {
+	var auth = firebase.auth();
+	var emailAddress = $('#email').val();
+
+	auth.sendPasswordResetEmail(emailAddress)
+		.then(function () {
+			alert('Se ha enviado un correo a su cuenta. Por favor sigue los pasos indicados.');
+		}, function (error) {
+			console.log(error)
+		})
+}
+
+
+var fbLogin = function () {
+	//Facebook login actions...
+}
